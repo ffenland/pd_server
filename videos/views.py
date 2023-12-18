390c420885b1b896794284c4a36ee02bd4cc9c84
ffenland <ffenland@gmail.com>
2023-12-18 12:12:06 +0900

    dd

--- videos/views.py
+++ videos/views.py
@@ -9,6 +9,8 @@ from django.conf import settings
 from firebase_admin import auth, db
 from .models import Video
 
+static_path = "/static/videos/"
+
 
 def writeFirebaseDB(key_code, file_name, file_path):
     # firebase Database
@@ -24,18 +26,60 @@ def writeFirebaseDB(key_code, file_name, file_path):
 
 class VideoView(APIView):
     def get(self, request):
-        key_code = "31433516"
+        key_code = "405381"
         # Show user's all videos.
         # firebase
         ref = db.reference(f"files/{key_code}")
         snapshot = ref.order_by_key().get()
-        for key, val in snapshot.items():
-            if isinstance(val.get("path"), str):  # 'path' 키의 값이 문자열인 경우에만 처리
-                val["path"] = val["path"].replace("\\", "/")  # 문자열에서 역슬래시를 슬래시로 변경
+        if snapshot:
+            for key, val in snapshot.items():
+                if isinstance(val.get("path"), str):  # 'path' 키의 값이 문자열인 경우에만 처리
+                    val["path"] = val["path"].replace("\\", "/")  # 문자열에서 역슬래시를 슬래시로 변경
+                state = val.get("state")
+                path = val.get("path")
+                print(f"{key} is {state}, {path}")
+            return Response({"ok": True})
+        else:
+            return Response({"ok": False})
 
-            print(f"{key} is {val}")
+    def post(self, request):
+        # handle uploaded video file.
+        file = request.data.get("file")
+        key_code = request.data.get("key_code")
+        if file and request.user and key_code:
+            try:
+                user = request.user
+                upload_path = os.path.join("static", "videos", f"{user.key_code}")
+                os.makedirs(upload_path, exist_ok=True)
+                # 중복 파일명 체크
+                file_name = file.name
+                file_path = os.path.join(upload_path, file_name)
+                count = 1
+                while os.path.exists(file_path):
+                    # 중복되는 경우 숫자를 추가하여 파일 이름 변경
+                    file_name = f"{os.path.splitext(file.name)[0]}_{count}{os.path.splitext(file.name)[1]}"
+                    file_path = os.path.join(upload_path, file_name)
+                    count += 1
 
-        return Response({"ok": True})
+                # 파일 저장
+                with open(os.path.join(upload_path, file_name), "wb+") as destination:
+                    for chunk in file.chunks():
+                        destination.write(chunk)
+
+                # firebase Database
+
+                writeFirebaseDB(
+                    key_code=key_code,
+                    file_name=file_name,
+                    file_path=f"static/videos/{user.key_code}",
+                )
+
+                return Response({"ok": True})
+            except Exception:
+                return Response({"ok": False})
+
+        else:
+            return Response({"ok": False})
 
 
 class VideoUploadView(APIView):
